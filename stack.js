75df97d2394c9b5033a86b274916f4d7a50fe510
wspandihai <2711002357@qq.com>
2018-04-27 17:05:27 +0800

    add
    Signed-off-by: wspandihai <2711002357@qq.com>

---
+++ stack.js
@@ -0,0 +1,16 @@
+function Stack() {
+  this.storege = '';
+  this.splitTag = '#';
+}
+
+Stack.prototype.push = function (value) {
+  
+}
+
+Stack.prototype.pop = function () {
+
+}
+
+Stack.prototype.size = function () {
+
+}

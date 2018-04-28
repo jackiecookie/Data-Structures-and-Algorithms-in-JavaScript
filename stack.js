fb1d596a69c1637f3114562f76e7293d36a683a2
wspandihai <2711002357@qq.com>
2018-04-28 08:46:26 +0800

    comit
    Signed-off-by: wspandihai <2711002357@qq.com>

--- stack.js
+++ stack.js
@@ -1,16 +1,32 @@
-function Stack() {
-  this.storege = '';
-  this.splitTag = '#';
+function Stack(capacity) {
+  this._storage = {};
+  this._capacity = capacity || 0;
+  this._count = 0;
 }
 
+// O(1)
 Stack.prototype.push = function (value) {
-  
-}
+  if (this._count < this._capacity) {
+    this._storage[this._count++] = value;
+    return this._count;
+  }
+};
 
+// O(1)
 Stack.prototype.pop = function () {
+  if (this._count > 0) {
+    var result = this.peek();
+    delete this._storage[--this._count];
+    return result;
+  }
+};
 
-}
+// O(1)
+Stack.prototype.peek = function () {
+  return this._storage[this._count - 1]
+};
 
-Stack.prototype.size = function () {
-
-}
+// O(1)
+Stack.prototype.count = function () {
+  return _count;
+};
